775cdb931d03dbba463e6c1859396e2cab8911cf
Unknown <Asher.parry@icloud.com>
2018-06-25 16:58:04 +1000

    update to 2.1.301

--- DiscordianServer/Hubs/ChatChainHub.cs
+++ DiscordianServer/Hubs/ChatChainHub.cs
@@ -16,16 +16,21 @@ namespace DiscordianServer.Hubs
             this.logger = logger;
         }
 
-        public async Task GenericAnyDiscordChatMessage(string channel, string author, string message)
-        {
-            logger.LogInformation($"Discord Chat Message: {author} in channel {channel} - {message}");
-            await Clients.All.SendAsync("GenericAnyDiscordChatMessage", channel, author, message);
-        }
-
         // ClientType is what ChatChain extension is connecting. E.G. "ChatChainDC", These should be Unique!
         // ClientName is the name of the specific cleitn connecting. E.G. "Minecolonies Test Server", These should be Unique!
         // Channel is used to specify a chat channel. E.G. "staff" channel.
 
+        public async Task SendCommand(string clientType, string clientName, string channel, string user, string destinationClient, string command)
+        {
+            logger.LogInformation($"Client Type: {clientType} of Name: {clientName} had user: {user} in channel: {channel} send command: {command} to client: {destinationClient}");
+            await Clients.All.SendAsync("SendCommand", clientType, clientName, channel, user, destinationClient, command);
+        }
+
+        public async Task CommandResponse(string clientType, string clientName, string channel, string user, string destinationClient, string command, string response)
+        {
+            logger.LogInformation($"Client Type: {clientType} of Name: {clientName} had user: {user} in channel: {channel} respond to command: {command} from client: {destinationClient} with response: {response}");
+            await Clients.All.SendAsync("SendCommand", clientType, clientName, channel, user, destinationClient, command, response);
+        }
         public async Task RequestJoined(string clientType, string clientName, string requestedClient)
         {
             logger.LogInformation($"Client Type: {clientType} of Name: {clientName} requested a joined list from: {requestedClient}");
