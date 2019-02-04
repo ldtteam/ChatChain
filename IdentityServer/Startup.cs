1524cf1fe18a4690bde697b57212d1bf35a4b166
Asherslab <Asher.parry@icloud.com>
2019-02-04 17:27:06 -0600

    testing2

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -84,7 +84,7 @@ namespace IdentityServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            InitializeDatabase(app);
+            //InitializeDatabase(app);
             
             if (env.IsDevelopment())
             {
