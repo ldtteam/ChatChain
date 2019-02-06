50ec7ed614396e71bea7f61f609d23abb42465f7
Asherslab <Asher.parry@icloud.com>
2019-02-05 22:17:44 -0600

    testing9 holy cow

---
+++ IdentityServer WebApp/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+namespace IdentityServer_WebApp
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
