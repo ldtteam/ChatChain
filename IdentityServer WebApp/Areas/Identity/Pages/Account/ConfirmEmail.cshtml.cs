c651bce4ea80173c415227d8ab56b63b254c5bb8
Asherslab <asher.parry@icloud.com>
2019-02-27 23:51:38 -0600

    testing63

--- IdentityServer WebApp/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ IdentityServer WebApp/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer_WebApp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,9 +13,9 @@ namespace IdentityServer_WebApp.Areas.Identity.Pages.Account
     [AllowAnonymous]
     public class ConfirmEmailModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ConfirmEmailModel(UserManager<IdentityUser> userManager)
+        public ConfirmEmailModel(UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
         }
