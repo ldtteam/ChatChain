ece930f9b9ffe2b60934c4e10b76a1deb3f1d39b
Asherslab <asher.parry@icloud.com>
2019-02-27 20:51:52 -0600

    testing57

---
+++ ChatChainServer/Models/MessageObjects/UserEventMessage.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ChatChainServer.Models.MessageObjects
+{
+    public class UserEventMessage
+    {
+        public string Event { get; set; }
+        public User User { get; set; }
+        public Client SendingClient { get; set; }
+        public bool SendToSelf { get; set; }
+    }
+}
