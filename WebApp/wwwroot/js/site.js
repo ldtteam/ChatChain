48e813597dd72a0acdfee95d414880e778f43cd4
Snaddyvitch-Dispenser <28988626+Snaddyvitch-Dispenser@users.noreply.github.com>
2019-04-14 21:08:16 +0100

    Second Section of The Home Page

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+// Mobile Responsiveness
+document.addEventListener("DOMContentLoaded", function () { var t = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0); 0 < t.length && t.forEach(function (n) { n.addEventListener("click", function () { var t = n.dataset.target, e = document.getElementById(t); n.classList.toggle("is-active"), e.classList.toggle("is-active") }) }) }); 
