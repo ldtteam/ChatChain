aa38be86892bee57f3a5362cd46aadcfc475f7f3
Asherslab <asher.parry@icloud.com>
2019-04-30 19:52:37 -0500

    Add ClientRank class and add it to User class

---
+++ ChatChainServer/Models/ClientRank.cs
@@ -0,0 +1,11 @@
+namespace ChatChainServer.Models
+{
+    public class ClientRank
+    {
+        public string Name { get; set; }
+        public string UniqueId { get; set; }
+        public int Priority { get; set; }
+        public string Display { get; set; }
+        public string Colour { get; set; }
+    }
+}
--- ChatChainServer/Models/User.cs
+++ ChatChainServer/Models/User.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+
 namespace ChatChainServer.Models
 {
     public class User
     {
         public string Name { get; set; }
+        public string UniqueId { get; set; }
+        
+        public List<ClientRank> ClientRanks { get; set; }
+        
     }
 }
