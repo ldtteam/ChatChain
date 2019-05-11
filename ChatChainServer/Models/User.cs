4a84aee537c709f312e0c162b56d5620ce8dada0
Asherslab <asher.parry@icloud.com>
2019-05-11 15:39:18 -0500

    Add optional NickName object to User object

--- ChatChainServer/Models/User.cs
+++ ChatChainServer/Models/User.cs
@@ -6,6 +6,7 @@ namespace ChatChainServer.Models
     {
         public string Name { get; set; }
         public string UniqueId { get; set; }
+        public string NickName { get; set; }
         
         public List<ClientRank> ClientRanks { get; set; }
         
