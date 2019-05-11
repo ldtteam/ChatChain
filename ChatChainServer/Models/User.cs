d4645b062efee88631e61dc6db982058565b963f
Asherslab <asher.parry@icloud.com>
2019-05-11 18:32:37 -0500

    Add colour object to User object

--- ChatChainServer/Models/User.cs
+++ ChatChainServer/Models/User.cs
@@ -7,6 +7,7 @@ namespace ChatChainServer.Models
         public string Name { get; set; }
         public string UniqueId { get; set; }
         public string NickName { get; set; }
+        public string Colour { get; set; }
         
         public List<ClientRank> ClientRanks { get; set; }
         
