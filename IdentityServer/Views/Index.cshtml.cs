de835744b2f666271a064039d5c6a3e893ba70f6
Asherslab <asher.parry@icloud.com>
2019-05-16 19:52:56 -0500

    Testing login, register, and index pages

---
+++ IdentityServer/Views/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServer.Views
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
